758a35f6b139866318f174ed6e4e569bf1159438
Timo Bihlmaier <info@timobihlmaier.de>
2023-01-12 11:18:01 +0100

    added product details page

---
+++ Website/assets/js/productselector.js
@@ -0,0 +1,36 @@
+function selectType(element) {
+    let types = document.querySelectorAll('.type-button');
+    types.forEach(type => type.classList.remove('active'));
+    element.classList.add("active");
+    changeSelection();
+}
+
+function selectSize(element) {
+    let sizes = document.querySelectorAll('.size-button');
+    sizes.forEach(size => size.classList.remove('active'));
+    element.classList.add("active");
+    changeSelection();
+}
+
+function changeSelection() {
+    var selectedType = document.querySelector("button.type-button.active").dataset.type;
+    var selectedSize = document.querySelector("button.size-button.active").innerHTML;
+    var productName = document.querySelector("#product-name");
+    var productPrice = document.querySelector("#product-price");
+
+    var prices = {
+        "T-Shirt": {
+            "Small": 10,
+            "Medium": 15,
+            "Large": 20
+        },
+        "Hoodie": {
+            "Small": 30,
+            "Medium": 35,
+            "Large": 40
+        }
+    }
+
+    productName.innerHTML = selectedType + " - " + selectedSize;
+    productPrice.innerHTML = "$" + prices[selectedType][selectedSize];
+}
